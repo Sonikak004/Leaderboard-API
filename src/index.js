ac9bb29050c948655223e6af2d6047b4b0aa1063
sonika <sonikak004@gmail.com>
2023-06-29 07:52:04 +0100

    Modify index.js file with eventlistner for buttons and call functions when clicked

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './style.css';
+import { refreshScore } from './modules/refreshScore.js';
+import { submitScore } from './modules/submitScore.js';
+
+export const URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
+export const gameId = 'ARAId6sN2LKi2AeYJNFw';
+
+const refresh = document.querySelector('#refreshButton');
+const submit = document.querySelector('#submitForm');
+refresh.addEventListener('click', refreshScore);
+submit.addEventListener('submit', submitScore);
